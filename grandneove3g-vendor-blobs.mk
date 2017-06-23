54c5d0d5201b4ecd4a7dc9b2ed138a1a66f21982
HA$HT@G <hasherror404@gmail.com>
2017-06-23 20:40:29 +0530

    initial commit
    Signed-off-by: HA$HT@G <hasherror404@gmail.com>

---
+++ grandneove3g-vendor-blobs.mk
@@ -0,0 +1,71 @@
+#
+# Copyright (C) 2016 Android Open Source Project
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PROPRIETARY_FILES := \
+	bin/at_distributor \
+	bin/engpc \
+	bin/ext_data.sh \
+	bin/ext_kill.sh \
+	bin/ext_symlink.sh \
+	bin/gps.cer \
+	bin/gpsd \
+	bin/macloader \
+	bin/modemd \
+	bin/nvitemd \
+	bin/phoneserver \
+	bin/prepare_param.sh \
+	bin/refnotify \
+	bin/rild \
+	etc/gps.conf \
+	etc/wifi/bcmdhd_apsta.bin \
+	etc/wifi/bcmdhd_mfg.bin \
+	etc/wifi/bcmdhd_sta.bin \
+	etc/wifi/nvram_mfg.txt \
+	etc/wifi/nvram_net.txt \
+	etc/wifi/p2p_supplicant_overlay.conf \
+	etc/wifi/wpa_supplicant_overlay.conf \
+	etc/wifi/wpa_supplicant.conf \
+	lib/egl/libGLES_mali.so \
+	lib/hw/gps.default.so \
+	lib/hw/gralloc.sc8830.so \
+	lib/hw/sensors.sc8830.so \
+	lib/libatchannel.so \
+	lib/libatparser.so \
+	lib/libboost.so \
+	lib/libfactoryutil.so \
+	lib/libisp.so \
+	lib/libmorpho_easy_hdr.so \
+	lib/libomission_avoidance.so \
+	lib/libomx_aacdec_sprd.so \
+	lib/libomx_avcdec_hw_sprd.so \
+	lib/libomx_avcenc_hw_sprd.so \
+	lib/libomx_m4vh263dec_hw_sprd.so \
+	lib/libomx_m4vh263enc_hw_sprd.so \
+	lib/libomx_mp3dec_sprd.so \
+	lib/libomx_vpxdec_hw_sprd.so \
+	lib/libreference-ril_sp.so \
+	lib/libril.so \
+	lib/libsecnativefeature.so \
+	lib/libsecril-client.so \
+	lib/libsprd_agps_agent.so \
+	lib/libuvdenoise.so \
+	vendor/firmware/BCM4343A0_001.001.034.0056.0197_M_ORC_Core3.hcd \
+	vendor/firmware/vbc_eq \
+	vendor/lib/libbt-vendor.so \
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(PROPRIETARY_FILES),vendor/samsung/grandneove3g/proprietary/$(f):system/$(f))
