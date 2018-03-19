0a720a1051a999211553d1747227b511d0ce46a2
HASH <hasherror404@gmail.com>
2018-03-19 19:57:57 +0530

    Grandneove3g: Bringup lineage-14.1
    Signed-off-by: HASH <hasherror404@gmail.com>

--- grandneove3g-vendor-blobs.mk
+++ grandneove3g-vendor-blobs.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2016 Android Open Source Project
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2017 Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,50 +14,15 @@
 # limitations under the License.
 #
 
-PROPRIETARY_FILES := \
-	bin/at_distributor \
-	bin/engpc \
-	bin/ext_data.sh \
-	bin/ext_kill.sh \
-	bin/ext_symlink.sh \
-	bin/gps.cer \
-	bin/gpsd \
-	bin/macloader \
-	bin/modemd \
-	bin/nvitemd \
-	bin/phoneserver \
-	bin/prepare_param.sh \
-	bin/refnotify \
-	bin/rild \
-	etc/gps.conf \
-	lib/egl/libGLES_mali.so \
-	lib/hw/camera.sc8830.so \
-	lib/hw/gps.default.so \
-	lib/hw/gralloc.sc8830.so \
-	lib/hw/sensors.sc8830.so \
-	lib/libatchannel.so \
-	lib/libatparser.so \
-	lib/libboost.so \
-	lib/libfactoryutil.so \
-	lib/libisp.so \
-	lib/libmorpho_easy_hdr.so \
-	lib/libomission_avoidance.so \
-	lib/libomx_aacdec_sprd.so \
-	lib/libomx_avcdec_hw_sprd.so \
-	lib/libomx_avcenc_hw_sprd.so \
-	lib/libomx_m4vh263dec_hw_sprd.so \
-	lib/libomx_m4vh263enc_hw_sprd.so \
-	lib/libomx_mp3dec_sprd.so \
-	lib/libomx_vpxdec_hw_sprd.so \
-	lib/libreference-ril_sp.so \
-	lib/libril.so \
-	lib/libsecnativefeature.so \
-	lib/libsecril-client.so \
-	lib/libsprd_agps_agent.so \
-	lib/libuvdenoise.so \
-	vendor/firmware/BCM43430A1_001.002.009.0013.0018_ORC_GrandNeo_VE.hcd \
-	vendor/firmware/vbc_eq \
-	vendor/lib/libbt-vendor.so \
-
 PRODUCT_COPY_FILES += \
-	$(foreach f,$(PROPRIETARY_FILES),vendor/samsung/grandneove3g/proprietary/$(f):system/$(f))
+    vendor/samsung/grandneove3g/proprietary/vendor/firmware/BCM43430A1_001.002.009.0013.0018_ORC_GrandNeo_VE.hcd:system/vendor/firmware/BCM43430A1_001.002.009.0013.0018_ORC_GrandNeo_VE.hcd \
+    vendor/samsung/grandneove3g/proprietary/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
+    vendor/samsung/grandneove3g/proprietary/etc/wifi/bcmdhd_sta.bin:system/etc/wifi/bcmdhd_sta.bin \
+    vendor/samsung/grandneove3g/proprietary/etc/wifi/bcmdhd_mfg.bin:system/etc/wifi/bcmdhd_mfg.bin \
+    vendor/samsung/grandneove3g/proprietary/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    vendor/samsung/grandneove3g/proprietary/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    vendor/samsung/grandneove3g/proprietary/lib/libisp.so:system/lib/libisp.so \
+    vendor/samsung/grandneove3g/proprietary/lib/libmorpho_easy_hdr.so:system/lib/libmorpho_easy_hdr.so \
+    vendor/samsung/grandneove3g/proprietary/lib/libuvdenoise.so:system/lib/libuvdenoise.so \
+    vendor/samsung/grandneove3g/proprietary/lib/hw/camera.sc8830.so:system/lib/hw/camera.sc8830.so \
+    vendor/samsung/grandneove3g/proprietary/lib/hw/sensors.sc8830.so:system/lib/hw/sensors.sc8830.so \
